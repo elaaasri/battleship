02e5d0cbac837af89a38b5fec50d34474b1b9e98
elaaasri <anas_elasri@outlook.fr>
2024-07-11 17:52:10 +0100

    adjust receiveAttack func to keep track of missed attacks

--- src/models/gameBoard.js
+++ src/models/gameBoard.js
@@ -12,10 +12,9 @@ class GameBoard {
     for (let i = 0; i < this.rows; i++) {
       this.board[i] = [];
       for (let j = 0; j < this.columns; j++) {
-        this.board[i][j] = 0;
+        this.board[i][j] = { value: 0, missed: null };
       }
     }
-    return this.board;
   }
   // place the ship in the correct coordinates :
   placeShip(row, column) {
@@ -33,10 +32,13 @@ class GameBoard {
   // checks if the attack hits a ship or not :
   receiveAttack(row, column) {
     const attackedCoordinates = this.board[row][column];
-    if (attackedCoordinates != 0) {
-      attackedCoordinates.hit();
+    if (attackedCoordinates.value != 0) {
+      attackedCoordinates.hit(); // increases attacked ship hits.
       return "attack hits the ship!";
-    } else return "attack missed the ship!";
+    } else {
+      attackedCoordinates.missed = true; // tracks missed attacks.
+      return "attack missed the ship!";
+    }
   }
 }
 export default GameBoard;
--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -6,14 +6,7 @@ test("place ship", () => {
   expect(board.placeShip(11, 15)).toBe("out of bound coordinates!");
 });
 
-test("receive attack", () => {
-  const board = new GameBoard();
-  board.placeShip(9, 5); // places a ship for test.
-  expect(board.receiveAttack(9, 5)).toBe("attack hits the ship!"); // same coordinates as the testing ship.
-  expect(board.receiveAttack(9, 6)).toBe("attack missed the ship!"); // different coordinates from testing ship.
-});
-
-test("receive attack", () => {
+test("receive attacks and track missed ones", () => {
   const board = new GameBoard();
   board.placeShip(9, 5); // places a ship for test.
   expect(board.receiveAttack(9, 5)).toBe("attack hits the ship!"); // same coordinates as the testing ship.
